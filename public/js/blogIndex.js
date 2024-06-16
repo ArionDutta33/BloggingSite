284aa7b02822e473f6e31885ef7268194be5a14f
ArionDutta33 <ariondutta333@outlook.com>
2024-06-16 22:49:27 +0530

    Removed the cursor animation and added a navbar animation

--- public/js/blogIndex.js
+++ public/js/blogIndex.js
@@ -1,11 +1,7 @@
-const cursor = document.querySelector(".cursor")
-const container = document.querySelector(".container")
-const cursorAnimation = () => {
-    container.addEventListener("mousemove", (e) => {
-        gsap.to(cursor, {
-            left: e.x - 60,
-            top: e.y - 360
-        })
-    })
-}
-cursorAnimation()
+const logo = document.querySelector(".logo")
+const searchBlog = document.querySelector("input")
+const navlinks = document.querySelector(".navlinks")
+gsap.from([logo, searchBlog, navlinks], {
+    y: -100,
+    stagger: 1
+})
