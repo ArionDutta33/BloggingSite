ec1fef73ec920603a4a9c4d0ca507ff569cb84ab
ArionDutta33 <ariondutta333@outlook.com>
2024-06-17 18:35:07 +0530

    set up middle ware for validating the blogs

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 const Blog = require("./models/blogs")
-
+const ExpressError = require("./utils/ExpressError")
+const { blogSchema } = require("./schemas")
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl; // add this line
@@ -25,4 +26,16 @@ module.exports.isAuthor = async (req, res, next) => {
         return res.redirect(`/blogs/${blog._id}`)
     }
     next()
+}
+
+
+
+module.exports.validateBlog = (req, res, next) => {
+    const { error } = blogSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 404)
+    } else {
+        next()
+    }
 }
