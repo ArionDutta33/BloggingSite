347d58fd921a7cb7695a39faf36f265d5d63bc23
ArionDutta33 <ariondutta333@outlook.com>
2024-06-17 13:29:08 +0530

    Set up session and passort js for auth

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const mongoose = require("mongoose")
 const app = express();
+const Async = require("./utils/Async")
+const ExpressError = require("./utils/ExpressError")
 const path = require('path');
+const session = require("express-session")
+const methodOverride = require("method-override")
 const ejsMate = require('ejs-mate');
 const Blog = require("./models/blogs")
+const passport = require("passport")
+const LocalStrategy = require("passport-local")
+const User = require("./models/user");
+
+//routes
+
+
+
+const blogRoutes = require("./routes/blog")
+
+
+
+
+
+//end
+
 mongoose.connect("mongodb://127.0.0.1:27017/Blog").then(() => {
     console.log("MONGODB CONNECTED")
 })
@@ -15,9 +35,29 @@ app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+const sessionConfig = {
+    secret: "password",
+    resave: false,
+    saveUninitialized: true,
+    Cookie: {
+        httpOnly: true,
+        expires: Date.now() * 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
+//flash
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 app.use(express.urlencoded({
     extended: true
 }));
+//method override
+app.use(methodOverride('_method'))
 // app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -25,38 +65,30 @@ app.use(express.static(path.join(__dirname, 'public')))
 //     // console.log(req)
 //     next()
 // })
+//fake user
+app.get("/fakeuser", async (req, res) => {
+    const user = new User({ email: "email@email.com", username: "John Doe" })
+    const registeredUser = await User.register(user, "password")
+    res.send(registeredUser)
+})
 
 app.get("/", (req, res) => {
-    res.send("homepage")
+    res.render("index")
 })
+app.use("/blogs", blogRoutes)
 
-app.get('/blogs', async (req, res) => {
-    const blogs = await Blog.find({})
-    res.render('blogs/index', { blogs })
-});
-app.get("/blogs/new", (req, res) => {
-    res.render("blogs/new")
-})
 
-//post create new blog
-app.post("/blogs", async (req, res) => {
-
-    try {
-        const blog = new Blog(req.body.blogs)
-        await blog.save()
-        res.redirect("/blogs")
-    } catch (e) {
-        console.log("ERROR ERROR ERROR ERROR ------>>>>>>", e)
-    }
+app.all("*", (req, res, next) => {
+    next(new ExpressError("Page not found", 404))
 })
-//show the individual blog
-app.get("/blogs/:id", async (req, res) => {
-    const { id } = req.params
-    const blog = await Blog.findById(id)
-    res.render("blogs/show", { blog })
-})
-app.get("/blogs/:id/edit", (req, res) => {
-    res.send("edit")
+app.use((err, req, res, next) => {
+    const {
+        statusCode = 500
+    } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render("error", {
+        err
+    })
 })
 app.listen(3000, () => {
     console.log("SERVER RUNNING")
