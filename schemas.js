75d462a3fdf127fd1875f7b023c20216b5ddac1d
ArionDutta33 <ariondutta333@outlook.com>
2024-06-18 20:58:04 +0530

    joi

--- schemas.js
+++ schemas.js
@@ -1,20 +1,44 @@
-const Joi = require("joi")
+const BaseJoi = require("joi")
+const sanitizeHtml=require("sanitize-html")
+const { validate } = require("./models/blogs")
+const { sanitizeFilter } = require("mongoose")
 
-module.exports.spotDefinition = Joi.object({
-    blogs: Joi.object({
+// module.exports.spotDefinition = Joi.object({
+//     blogs: Joi.object({
 
-        title: Joi.string().required().max(20),
-        body: Joi.string().required().max(100),
+//         title: Joi.string().required().max(20),
+//         body: Joi.string().required().max(100),
 
 
-    }).required()
+//     }).required()
+// })
+
+const extension=(joi)=>({
+    type:'string',
+    base:joi.string(),
+    messages:{
+        'string.escapeHTML': '{{label}} must not include HTML'
+    },
+    rules:{
+        escapeHTML:{
+            validate(value,helpers){
+                const clean=sanitizeHtml(value,{
+                    allowedTags:[],
+                    allowedAttributes:{},
+                });
+                if(clean!==value) return helpers.error('string.escapeHTML',{value})
+                    return clean;
+            }
+        }
+    }
 })
 
+const Joi=BaseJoi.extend(extension)
 
 
 module.exports.blogSchema = Joi.object({
     blogs: Joi.object({
-        title: Joi.string().max(20).required(),
-        body: Joi.string().required()
+        title: Joi.string().max(20).required().escapeHTML(),
+        body: Joi.string().required().escapeHTML()
     }).required()
 })
