ebde23547f4072034c792c401c6e54687855680b
ArionDutta33 <ariondutta333@outlook.com>
2024-06-18 21:14:01 +0530

    refactored working

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV!=="production"){
+    require("dotenv").config()
+}
+
 const express = require('express');
 const mongoose = require("mongoose")
 const app = express();
@@ -13,20 +17,22 @@ const Blog = require("./models/blogs")
 const passport = require("passport")
 const LocalStrategy = require("passport-local")
 const User = require("./models/user");
-
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet=require("helmet")
+const db_url=process.env.DB_URL
 //routes
-
 
 
 const blogRoutes = require("./routes/blog")
 
 
-const userRoutes = require("./routes/user")
+const userRoutes = require("./routes/user");
+const { object } = require('joi');
 
 
 //end
 
-mongoose.connect("mongodb://127.0.0.1:27017/Blog").then(() => {
+mongoose.connect(db_url).then(() => {
     console.log("MONGODB CONNECTED")
 })
     .catch((e) => {
@@ -38,17 +44,64 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 const sessionConfig = {
+    name:"session",
     secret: "password",
     resave: false,
     saveUninitialized: true,
     Cookie: {
         httpOnly: true,
+        secure:true,//cookies are accessible through https
         expires: Date.now() * 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
 app.use(session(sessionConfig))
 app.use(flash());
+app.use(helmet( ))
+
+const scriptSrcUrls = [
+    "'unsafe-inline'",
+    "'self'",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js",
+    "https://cdn.tiny.cloud/1/gajuheuruiqplk071bg268yxvuipk6aj30mbrf5912k1ghax/tinymce/7/tinymce.min.js"
+];
+
+const styleSrcUrls = ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"];
+
+const fontSrcUrls = [
+    "'self'",
+    "https://fonts.googleapis.com"
+];
+
+const imgSrcUrls = [
+    "'self'",
+    "blob:",
+    "data:",
+    "https://unsplash.com/",
+    "https://images.unsplash.com/"
+];
+
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: [],
+            scriptSrc: [
+                "'self'",
+                "'unsafe-inline'",
+                "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css",
+                "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js",
+                "https://cdn.tiny.cloud/1/gajuheuruiqplk071bg268yxvuipk6aj30mbrf5912k1ghax/tinymce/7/tinymce.min.js"
+            ],
+            styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "https://cdnjs.cloudflare.com"],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: imgSrcUrls,
+            fontSrc: ["'self'", "https://fonts.googleapis.com", "https://fonts.gstatic.com", "https://cdnjs.cloudflare.com"]
+        },
+    },
+}));
+
 app.use(passport.initialize())
 app.use(passport.session())
 passport.use(new LocalStrategy(User.authenticate()))
@@ -57,6 +110,7 @@ passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
 app.use((req, res, next) => {
+    console.log(req.query)
     res.locals.currentUser = req.user
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
@@ -70,7 +124,7 @@ app.use(express.urlencoded({
 app.use(methodOverride('_method'))
 // app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
-
+app.use(mongoSanitize())
 // app.use((req, res, next) => {
 //     // console.log(req)
 //     next()
--- public/js/blogIndex.js
+++
@@ -1,7 +0,0 @@
-const logo = document.querySelector(".logo")
-const searchBlog = document.querySelector("input")
-const navlinks = document.querySelector(".navlinks")
-gsap.from([logo, searchBlog, navlinks], {
-    y: -100,
-    stagger: 1
-})
